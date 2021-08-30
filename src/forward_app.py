226cfa11a3cded591f610345026e087f20390479
Marcus Kertesz <ModernMAK@Hotmail.com>
2021-08-30 00:58:55 -0700

    Update forward_app.py
    Now throws a 401 (instead of 404) on the major Non-Get methods (Post/Patch/Put/Delete)
    
    Useful for telling users to not use HTTP

--- src/forward_app.py
+++ src/forward_app.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from starlette.exceptions import HTTPException
 from starlette.requests import Request
-from starlette.responses import RedirectResponse
+from starlette.responses import RedirectResponse, Response
 
 fwd_app = FastAPI(openapi_url=None)  # disable docs; not a rest-api but a webserve
 HTTP = "http"
 HTTPS = "https"
 MOVED_PERM = 301
 NOT_FOUND = 404
+UNAUTHORIZED = 401
 
 
 def forward_request(request: Request):
@@ -15,6 +16,10 @@ def forward_request(request: Request):
         url = request.url.replace(scheme=HTTPS)
         return RedirectResponse(url=url, status_code=MOVED_PERM, headers=request.headers)
     return HTTPException(NOT_FOUND, "The url is not using HTTP!")
+
+
+def block_request(request: Request):
+    return HTTPException(UNAUTHORIZED, "The url requires an HTTPS connection!")
 
 
 @fwd_app.get("/")
@@ -25,6 +30,22 @@ def forward_root_to_https(request: Request):
 @fwd_app.get("/{full_path}")
 def forward_path_to_https(request: Request, full_path: str):
     return forward_request(request)
+
+
+@fwd_app.post("/")
+@fwd_app.patch("/")
+@fwd_app.put("/")
+@fwd_app.delete("/")
+def block_root_to_https(request: Request):
+    return block_request(request)
+
+
+@fwd_app.patch("/{full_path}")
+@fwd_app.put("/{full_path}")
+@fwd_app.delete("/{full_path}")
+@fwd_app.get("/{full_path}")
+def block_path_to_https(request: Request, full_path: str):
+    return block_request(request)
 
 
 if __name__ == "__main__":
